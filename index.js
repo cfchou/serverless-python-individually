bb1e3d78bfbef78a652850ec7df3ff5efcc5e4a6
cfchou <cfchou@gmail.com>
2016-12-04 23:56:42 +0800

    customs in yaml

--- index.js
+++ index.js
@@ -11,16 +11,43 @@ const child_process = require('child_process');
 
 BbPromise.promisifyAll(fse);
 
-class ServerlessPythonMadeGreatAgain {
+class PythonMadeGreatAgain {
 
   beforePackaging() {
-    this.serverless.cli.log('beforePackaging...');
+    let that = this;
+    this.cli.log('beforePackaging...');
+    let promises = _.mapValues(this.serverless.service.functions, function (value, key) {
+      //return BbPromise.bind(that).then(_.partial(that.log, value, key));
+      return that.dummy(value, key);
+    });
+    return BbPromise.props(promises).then(function () {
+      let vs = _.values(promises);
+      for (let v of vs) {
+        that.cli.log('promise done...' + v.value().toString());
+      }
+      that.cli.log('done...');
+    });
+  };
 
-  }
+  dummy(value, key) {
+      this.cli.log('...' + key + ':' + value.toString());
+      return BbPromise.resolve(key)
+  };
 
   beforePackagingOne() {
     this.serverless.cli.log('beforePackagingOne...');
-  }
+    if (this.serverless.service.custom &&
+      this.serverless.service.custom.greatAgain) {
+      if (this.serverless.service.custom.greatAgain.cleanup) {
+        this.cleanup = this.serverless.service.custom.greatAgain.cleanup
+      }
+      if (this.serverless.service.custom.greatAgain.libSubDir) {
+        // TODO: throw if excluded
+        this.libSubDir = this.serverless.service.custom.greatAgain.libSubDir
+      }
+    }
+    this.serverless.cli.log('cleanup:');
+  };
 
   afterPackaging() {
     this.serverless.cli.log('afterPackaging...');
@@ -28,13 +55,24 @@ class ServerlessPythonMadeGreatAgain {
 
   afterPackagingOne() {
     this.serverless.cli.log('afterPackagingOne...');
-    if (this.serverless.service.custom &&
-      this.serverless.service.custom.spmga &&
-      this.serverless.service.custom.spmga.backup) {
-      this.serverless.cli.log('backup...');
-      cleanup()
-    }
   };
+
+  wrapping(wrapName, package_path, handler) {
+    const content = `
+# vim:fileencoding=utf-8
+# ${wrapName}.py
+# This file is generated on the fly by serverless-python-made-great-again plugin.
+import sys
+sys.path.append('${package_path}')
+import ${handler} as real_handler
+
+def handler(event, context):
+  return real_handler(event, context)
+ 
+`
+
+  }
+
 
   cleanup() {
     if (this.serverless.config.servicePath) {
@@ -50,6 +88,7 @@ class ServerlessPythonMadeGreatAgain {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
+    this.cli = serverless.cli
     this.hooks = {
       'before:deploy:createDeploymentArtifacts': () => BbPromise.bind(this)
         .then(this.beforePackaging),
@@ -63,8 +102,13 @@ class ServerlessPythonMadeGreatAgain {
       'after:deploy:function:packageFunction': () => BbPromise.bind(this)
         .then(this.afterPackagingOne),
     };
-    this.backup = undefined
+    // overwritten by custom.greatAgain.wrapName
+    this.wrapName = 'wrap';
+    // overwritten by custom.greatAgain.libSubDir
+    this.libSubDir = 'lib';
+    // overwritten by custom.greatAgain.cleanup
+    this.cleanup = true;
   };
 }
 
-module.exports = ServerlessPythonMadeGreatAgain;
+module.exports = PythonMadeGreatAgain;
