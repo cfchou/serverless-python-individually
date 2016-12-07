dfafe7466e92114d168bbf2c9915f7d9da48471d
Chifeng Chou <cfchou@gmail.com>
2016-12-07 18:34:14 +0800

    tmp

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const Fse = require('fs-extra');
 const ChildProcess = require('child_process');
 
 BbPromise.promisifyAll(Fse);
+BbPromise.promisifyAll(ChildProcess);
 
 
 function IgnorableError(message) {
@@ -28,8 +29,8 @@ class PythonMadeGreatAgain {
     const custom = this.serverless.service.custom;
     if (!_.has(custom, 'greatAgain')) {
       //throw new this.serverless.classes.Error(
-      throw new IgnorableError(
-        'Want to be great again you got to set up custom.greatAgain.');
+      return BbPromise.reject(new IgnorableError(
+        'Want to be great again you got to set up custom.greatAgain.'));
     }
     const greatAgain = custom.greatAgain;
     if (_.has(greatAgain, 'wrapName') && greatAgain.wrapName) {
@@ -44,27 +45,26 @@ class PythonMadeGreatAgain {
       this.cleanup = greatAgain.cleanup;
     }
     this.log('cleanup: ' + this.cleanup);
+    return BbPromise.resolve();
   };
 
   selectOne() {
-    const functions = this.serverless.service.functions;
     const greatAgain = this.serverless.service.custom.greatAgain;
     const target = this.options.function;
     const targetObj = this.options.functionObj;
     const targetKey = this.wrapName + ':' + target;
     const wrapper = this.wrapName + '.handler';
-    this.xxx = true;
 
     if (_.has(greatAgain, targetKey) &&
       _.endsWith(targetObj.handler, wrapper)) {
-      return {
+      return BbPromise.resolve({
         'name': target,
         'function': targetObj,
         'realHandler': greatAgain[targetKey]
-      }
+      })
     }
-    throw new IgnorableError(
-      'Want to be great again you got to set up custom.greatAgain.');
+    return BbPromise.reject(new IgnorableError(
+      'Want to be great again you got to set up custom.greatAgain.'));
   }
 
   selectAll() {
@@ -86,48 +86,54 @@ class PythonMadeGreatAgain {
     // selection
     return _.map(targetKeys, (targetKey) => {
       const target = targetKey.substring(prefixLen);
-      return {
+      return BbPromise.resolve({
         'name': target,
         'function': functions[target],
         'realHandler': greatAgain[targetKey]
-      }
+      })
     });
   }
 
+  /**
+   * Create a wrapper. Install packages.
+   * IgnorableError are ignored and replaced with BbPromise.resolve().
+   * @param target
+   * @returns {Promise.<undefined>}
+   */
   work(target) {
     const wrapper = this.wrapName + '.handler';
-    const handlerDir = target.function.handler.substring(0,
+    const wrapperPy = this.wrapName + '.py';
+    const wrapperDir = target.function.handler.substring(0,
       target.function.handler.length - wrapper.length);
-    const packagePath = Path.join(handlerDir, this.libSubDir);
-    const requirements = Path.join(handlerDir, 'requirements.txt');
+    const packagePath = Path.join(wrapperDir, this.libSubDir);
+    const requirements = Path.join(wrapperDir, 'requirements.txt');
 
-    let promise = this.wrap(handlerDir, this.wrapName + '.py', packagePath,
-      target.realHandler)
-    promise = promise.then(_.partial(_.bind(this.install, this), packagePath,
-      requirements));
-
-    this.works.push(promise);
-    return promise;
+    return this.wrap(wrapperDir, wrapperPy, packagePath, target.realHandler)
+      .then(_.partial(_.bind(this.fileAccessable, this), requirements))
+      //.then(_.partial(Fse.ensureDirAsync, packagePath))
+      .then(() => { return Fse.ensureDirAsync(packagePath)})
+      .then(_.partial(_.bind(this.install, this), packagePath, requirements))
+      .then(BbPromise.resolve,
+        _.partial(_.bind(this.catchIgnorableError, this), undefined));
   };
 
-  install(packagePath, requirements) {
-    const that = this;
-    return Fse.ensureDirAsync(packagePath)
-      .then(() => {
-          return Fse.accessAsync(requirements, Fse.constants.R_OK);
-      })
-      .then(() => {
-        that.log(requirements + ' exists');
-        const ret = ChildProcess.spawnSync('python', [
-          Path.resolve(__dirname, 'requirements.py'),
-          requirements, packagePath]);
-        that.log(ret.stderr.toString());
-        that.log(ret.stdout.toString());
-        return BbPromise.resolve();
-      }, () => {
-        return BbPromise.resolve();
+  /**
+   * Replace exceptions from fs.access with IgnorableError for not interrupting
+   * the promise chain.
+   * @param filename
+   * @returns {Promise.<undefined>|*}
+   */
+  fileAccessable(filename) {
+    return Fse.accessAsync(filename, Fse.constants.R_OK)
+      .then(BbPromise.resolve,
+        (err) => {
+          if (process.env.SLS_DEBUG) {
+            this.log(err.stack);
+          }
+          return BbPromise.reject(new IgnorableError(
+            'Can\'t access ' + filename));
       });
-  };
+  }
 
   wrap(dir, filename, packagePath, realHandler) {
     const content = `
@@ -145,13 +151,35 @@ def handler(event, context):
     return Fse.outputFileAsync(Path.join(dir, filename), content);
   };
 
+  install(packagePath, requirements) {
+    const ret = ChildProcess.spawnSync('python', [
+      Path.resolve(__dirname, 'requirements.py'),
+      requirements, packagePath]);
+    this.log(ret.stderr.toString());
+    this.log(ret.stdout.toString());
+    if (ret.error) {
+      return BbPromise.reject(res.error)
+    }
+    return BbPromise.resolve()
+  }
 
-  ignoreIgnorableError(e) {
+  /**
+   * Catch IgnorableError. Replace it with a promise resolved with value. So
+   * that the promise chain goes on.
+   * @param value
+   * @param e
+   * @returns {*}
+   */
+  catchIgnorableError(value, e) {
     if (e instanceof IgnorableError) {
-      // log then swallow
-      this.log(e.stack);
+      // log, then swallow
+      if (process.env.SLS_DEBUG) {
+        this.log(e.stack);
+      } else {
+        this.log('IgnorableError: ' + e.message)
+      }
       // NOTE: following functions in the promise chain will be executed.
-      return BbPromise.resolve();
+      return BbPromise.resolve(value);
     } else {
       //throw new this.serverless.classes.Error(e.message);
       throw e;
@@ -160,8 +188,6 @@ def handler(event, context):
 
   clean() {
     this.log('cleaning...')
-    BbPromise.resolve(this.works).then(() => {
-    })
   };
 
 
@@ -169,35 +195,45 @@ def handler(event, context):
     this.serverless = serverless;
     this.options = options;
     this.log = (msg) => { serverless.cli.log('[GreatAgain] ' + msg); };
-    // array of promises
-    this.works = [];
-    this.hooks = {
-      'before:deploy:createDeploymentArtifacts': () => BbPromise.bind(this)
-        .then(this.overwriteDefault)
-        .then(this.selectAll)
-        .map(this.work)
-        .then(BbPromise.resolve, this.ignoreIgnorableError),
-
-
-      'after:deploy:createDeploymentArtifacts': () => BbPromise.bind(this)
-        .then(this.clean),
-
-      'before:deploy:function:packageFunction': () => BbPromise.bind(this)
-        .then(this.overwriteDefault)
-        .then(this.selectOne)
-        .then(this.work)
-        .then(BbPromise.resolve, this.ignoreIgnorableError),
-
-      'after:deploy:function:packageFunction': () => BbPromise.bind(this)
-        .then(this.clean),
-    };
     // overwritten by custom.greatAgain.wrapName
     this.wrapName = 'wrap';
     // overwritten by custom.greatAgain.libSubDir
     this.libSubDir = 'lib';
     // overwritten by custom.greatAgain.cleanup
     this.cleanup = true;
+    this.hooks = {
+      'before:deploy:createDeploymentArtifacts': () => BbPromise.bind(this)
+        .then(this.overwriteDefault)
+        .then(this.selectAll)
+        .map(this.work),
 
+      'after:deploy:createDeploymentArtifacts': () => BbPromise.bind(this)
+        .then(_.bind(() => {
+          if (!this.cleanup) {
+            return BbPromise.reject(new IgnorableError('Cleanup is disabled'))
+          }
+          return BbPromise.resolve();
+        }, this))
+        .then(this.selectAll)
+        .map(this.clean)
+        .then(BbPromise.resolve, _.partial(this.catchIgnorableError, undefined)),
+
+      'before:deploy:function:packageFunction': () => BbPromise.bind(this)
+        .then(this.overwriteDefault)
+        .then(this.selectOne)
+        .then(this.work),
+
+      'after:deploy:function:packageFunction': () => BbPromise.bind(this)
+        .then(_.bind(() => {
+          if (!this.cleanup) {
+            // fall through until catch
+            throw new IgnorableError('Cleanup is disabled');
+          }
+          return BbPromise.resolve();
+        }, this))
+        .then(this.selectOne)
+        .then(this.clean, _.partial(this.catchIgnorableError, undefined)),
+    };
   };
 }
 
